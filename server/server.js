dbfeaab4860c886f76b3503e188989b3e717a696
SUNDARAVEL7203 <sundarvel.kasi@gmail.com>
2025-02-28 23:17:12 +0530

    bg-removal

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import express from "express"
 import cors from "cors"
 import connectDB from './configs/mongodb.js'
+import userRoutes from './routes/UserRoutes.js'
+
 
 //Appp config
 const PORT = process.env.PORT || 5000
@@ -12,6 +14,8 @@ connectDB()
 //Initialize Middlewares
 app.use(express.json())
 app.use(cors())
+app.use("/api/user", userRoutes)
+
 //API Routes
 app.get("/",(req,res) => {
     res.send("API Working")
